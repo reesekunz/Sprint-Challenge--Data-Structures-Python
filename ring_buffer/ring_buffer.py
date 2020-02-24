185741c89e7f2f3bc9ae609154e8f7d885f56bbe
Reese Kunz <reesekunz21@gmail.com>
2020-02-23 22:03:55 -0700

    restarting on ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,29 +6,17 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        self.order = dict()
 
     def append(self, item):
-        # Still room in DLL storage - Add item to head (defining head as newest element and tail as oldest)
-        if self.storage.length < self.capacity:
-            self.storage.add_to_head(item)
-            # newly added item becomes current
-            self.current = self.storage.head
-        # DLL storage is at capacity. Need to make room - remove from tail then add to head
-        elif self.storage.length == self.capacity:
-            self.storage.remove_from_tail()
-            self.storage.add_to_head(item)
-            if self.storage.head == self.current:
-                self.current = self.storage.tail
-            else:
-                self.current = self.current.next
+        pass
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-        # while list is not empty
-        while self.storage.length > 0:
-            list_buffer_contents.append(self.storage.head.value)
-            self.storage.head = self.storage.head.next
+
+        # TODO: Your code here
+
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
